51b08ffe1e2442be31ddaa88af0ae70609fed12a
Jin-Cheng, Lee <105055789+jinchunglee@users.noreply.github.com>
2025-05-09 18:48:23 +0800

    Update gpt_adapter.py

--- adapters/gpt_adapter.py
+++ adapters/gpt_adapter.py
@@ -1,6 +1,6 @@
 # from openai import OpenAI
 
-# client = OpenAI(api_key="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")
+# client = OpenAI(api_key="")
 
 # response = client.chat.completions.create(
 #     model="gpt-3.5-turbo",
